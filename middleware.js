60dbea449e2aeaacc45fe45e18afaec6f8b352cc
Rezk <audirezk@gmail.com>
2024-09-16 18:52:37 +0300

    add middleware file

---
+++ middleware.js
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export default function middleware(req) {
+
+  const isAuthenticated = cookies().get("authToken")?.value;
+  
+  // const publicRoutes = [
+  //   "/login",
+  //   "/fonts",
+  //   "/images",
+  //   "/scss",
+  //   "/_next",
+  //   "/favicon.ico",
+  // ];
+
+  // const protectedRoutes = ["/blogs", "/change-password", "/courses", "/courses-times", "/guest-users", "/reservations", "/subscribers"];
+  // const publicRoutes = ["/login"]
+
+  // const isPublicRoute = publicRoutes.some((route) =>
+  //   req?.nextUrl?.pathname.startsWith(route)
+  // );
+
+  // if (isPublicRoute) {
+  //   return NextResponse.next();
+  // }
+
+  if (!isAuthenticated) {
+    const absoluteUrl = new URL("/login", req.nextUrl.origin);
+    absoluteUrl.searchParams.set('redirect', req.nextUrl.pathname);
+    return NextResponse.redirect(absoluteUrl.toString());
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/((?!login|api|_next/static|_next/image|favicon.ico|fonts|images|scss|sitemap.xml|robots.txt).*)',
+  ],
+};
